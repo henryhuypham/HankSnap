6f82841ae98831f9f8d1fe27ec033451efcf887a
Huy Pham <huyphamthanh8290@gmail.com>
2016-10-22 21:37:07 +0700

    Show sent messages

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,18 @@
 module MessagesHelper
+  def recipient_name_list(message)
+    message.recipients.map { |recipient|
+      recipient.name.split.first
+    }.reduce('') { |result, name|
+      "#{result}#{result.present? ? ', ' : ''}#{name}"
+    }
+  end
+
+  def read_time(message)
+    if message.read?
+      read_time = message.updated_at
+      "#{read_time.strftime('%b')} #{read_time.strftime('%e')} - #{read_time.strftime('%l')}:#{read_time.strftime('%M')} #{read_time.strftime('%p')}"
+    else
+      'Unread'
+    end
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ApplicationRecord
   belongs_to :sender, class_name: 'User'
   has_and_belongs_to_many :recipients, class_name: 'User', join_table: 'messages_recipients', foreign_key: 'message_id'
 
-  enum staus: [:unread, :read]
+  enum status: [:unread, :read]
 end
