e72037a71a9144d0d4629781c5121748afabd844
Huy Pham <huyphamthanh8290@gmail.com>
2016-10-22 12:04:55 +0700

    User: add sign_in & sign_up actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def authenticate
   end
+
+  def sign_in
+
+  end
+
+  def sign_up
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = 'Welcome to HankSnap!'
+      session[:user_id] = @user.id
+      redirect_to
+    else
+      flash[:error] = "Error: #{@user.errors.full_messages.to_sentence}"
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/authenticate'
+  post  'sign_up', to: 'users#sign_up'
+  post  'sign_in', to: 'users#sign_in'
 
-  root 'users#authenticate'
+  root  'users#authenticate'
 end
