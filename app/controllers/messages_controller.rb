49b2ef533b248125ecc2c3bee32d7477bc1046f0
Huy Pham <huyphamthanh8290@gmail.com>
2016-10-23 03:30:36 +0700

    Message: view & mark as read

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   before_action :required_login
+  before_action :is_recipient_of_message, only: :view
 
   def inbox
     @messages = current_user.received_messages.order(created_at: :desc)
@@ -10,5 +11,23 @@ class MessagesController < ApplicationController
   end
 
   def compose
+  end
+
+  def view
+    @message = current_user.received_messages.find_by(id: params[:id])
+    if @message.present?
+      @is_read = @message.read?
+      @message.read!
+    end
+  end
+
+  private
+
+  def is_recipient_of_message
+    params.require(:id)
+    unless current_user.received_messages.exists?(id: params[:id])
+      flash[:error] = 'Error: message not found!'
+      redirect_to messages_inbox_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get   'messages/inbox', to: 'messages#inbox'
   get   'messages/sent', to: 'messages#sent'
   get   'messages/compose', to: 'messages#compose'
+  get   'messages/view', to: 'messages#view'
 
   post  'sign_up', to: 'users#sign_up'
   post  'sign_in', to: 'users#sign_in'
