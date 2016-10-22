4c90f162b4436f867a91b1b5820e2316382a5f0b
Huy Pham <huyphamthanh8290@gmail.com>
2016-10-22 17:29:05 +0700

    Set message status as enum

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :sender, class_name: 'User'
   has_and_belongs_to_many :recipients, class_name: 'User', join_table: 'messages_recipients', foreign_key: 'message_id'
+
+  enum staus: [:unread, :read]
 end
